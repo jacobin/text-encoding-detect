b7e639ed29c1d4957514fde3d1bea1edc250a9ac
Jacobin <harly.he@gmail.com>
2025-04-29 15:29:52 +0800

    _T

--- TextEncodingDetect-C++/TextEncodingDetect/main.cpp
+++ TextEncodingDetect-C++/TextEncodingDetect/main.cpp
@@ -23,20 +23,20 @@
 using namespace AutoIt::Common;
 
 
-int wmain(int argc, wchar_t* argv[])
+int _tmain(int argc, TCHAR* argv[])
 {
 	if (argc != 2)
 	{
-		wprintf(L"\nUsage: %s filename.", argv[0]);
+		_tprintf(_T("\nUsage: %s filename."), argv[0]);
 		return 1;
 	}
 	
 	// Open file in binary mode
-	FILE *file = _wfopen(argv[1], L"rb");
+	FILE *file = _tfopen(argv[1], _T("rb") );
 
 	if (file == NULL)
 	{
-		wprintf(L"\nCould not open file.\n");
+		_tprintf(_T("\nCould not open file.\n"));
 		return 1;
 	}
 
@@ -54,19 +54,19 @@ int wmain(int argc, wchar_t* argv[])
 	TextEncodingDetect textDetect;
 	TextEncodingDetect::Encoding encoding = textDetect.DetectEncoding(buffer, fsize);
 
-	wprintf(L"\nEncoding: ");
+	_tprintf(_T("\nEncoding: "));
 	if (encoding == TextEncodingDetect::None)
-		wprintf(L"Binary");
+		_tprintf(_T("Binary"));
 	else if (encoding == TextEncodingDetect::ASCII)
-		wprintf(L"ASCII (chars in the 0-127 range)");
+		_tprintf(_T("ASCII (chars in the 0-127 range)"));
 	else if (encoding == TextEncodingDetect::ANSI)
-		wprintf(L"ANSI (chars in the range 0-255 range)");
+		_tprintf(_T("ANSI (chars in the range 0-255 range)"));
 	else if (encoding == TextEncodingDetect::UTF8_BOM || encoding == TextEncodingDetect::UTF8_NOBOM)
-		wprintf(L"UTF-8");
+		_tprintf(_T("UTF-8"));
 	else if (encoding == TextEncodingDetect::UTF16_LE_BOM || encoding == TextEncodingDetect::UTF16_LE_NOBOM)
-		wprintf(L"UTF-16 Little Endian");
+		_tprintf(_T("UTF-16 Little Endian"));
 	else if (encoding == TextEncodingDetect::UTF16_BE_BOM || encoding == TextEncodingDetect::UTF16_BE_NOBOM)
-		wprintf(L"UTF-16 Big Endian");
+		_tprintf(_T("UTF-16 Big Endian"));
 
 	// Free up
 	delete[] buffer;
